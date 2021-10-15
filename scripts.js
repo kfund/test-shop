3754de6d66bb9642ec3e42214ce348c387b977b6
Denis Holubchikov <d.holubchikov@gmail.com>
2021-10-15 10:42:07 +0300

    added some fixes

--- scripts.js
+++ scripts.js
@@ -146,17 +146,6 @@ class App {
 
 }
 
-var active = document.getElementById("burger");
-// add Event Listener Click to Burger Icon Container
-active.addEventListener("click", function() {
-  //add or remove class "active" to Burger to start animation
-  this.classList.toggle("active");
-  //get menu-container by id
-  var menuShow = document.getElementById("menu");
-  //add or remove class "show" to show or hide menu and start its animations
-  menuShow.classList.toggle("show");
-});
-
 document.addEventListener('DOMContentLoaded', () => {
   const products = new App;
   products.getProducts()
